98cdcc2c386340c4a5220bd0fd9a67a5298ae0b4
回忆住事 <2577034655@qq.com>
2023-08-16 14:20:29 +0800

    first

---
+++ src/main/java/com/cj/jenkinstest/HelloController.java
@@ -0,0 +1,15 @@
+package com.cj.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello";
+    }
+
+}
---
+++ src/test/java/com/cj/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cj.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
